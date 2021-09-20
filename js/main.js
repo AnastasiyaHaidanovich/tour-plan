9ddc5b360742d884efebfb06cd2ddaa00fc5bc39
AnastasiyaHaidanovich <Nastyal0ve9517@gmail.com>
2021-09-20 18:07:44 +0300

    add: swiper moved by keyboard

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
+    autoheight: true,
+    watchOverflow: true,
 
     // Navigation arrows
     navigation: {
       nextEl: '.slider-button__next',
       prevEl: '.slider-button__prev',
     },
-    keyPress (swiper, keyCode){
-        
-    }
- 
+    
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
   });
