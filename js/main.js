532f6065fcb05d0dd95b79b1158dbb2c1a99649a
AnastasiyaHaidanovich <Nastyal0ve9517@gmail.com>
2021-09-20 16:22:49 +0300

    add: create swiper

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-button__next',
+      prevEl: '.slider-button__prev',
+    },
+    keyPress (swiper, keyCode){
+        
+    }
+ 
+  });
