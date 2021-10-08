1097d4770f8f9fbf463cd4be104b785defa23845
AnastasiyaHaidanovich <Nastyal0ve9517@gmail.com>
2021-10-08 16:35:44 +0300

    fix: form mistakes

--- js/main.js
+++ js/main.js
@@ -126,7 +126,7 @@ $(document).ready(function () {
     }
 
     $.validator.methods.email = function( value, element ) {
-      return this.optional( element ) || /[a-z]+@[a-z]+\.[a-z]+/.test( value );
+      return this.optional( element ) || /[a-z+0-9]+@[a-z]+\.[a-z]+/.test( value );
     }
     $(this).validate({
       rules: {
